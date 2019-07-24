03eb406f4ca725fb3451be99a716150e1d0cde6f
Jarrett Ang <jarrett_ang@hotmail.com>
2019-07-24 20:08:57 +0900

    Added new farmland design

---
+++ Flow/Assets/Scripts/_Managers/SceneChanger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void LoadFarmlandScene() {
+        SceneManager.LoadScene("FarmLand");
+    }
+
+    public void LoadTimerScene() {
+        SceneManager.LoadScene("Timer");
+    }
+}
