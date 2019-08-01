9e8d3ec99bc3efa9c8d796468b45efd1b06eacff
Jarrett Ang <jarrett_ang@hotmail.com>
2019-08-01 15:45:31 +0900

    Added privacy policy

---
+++ Flow/Assets/Scripts/Util/ExternalLinks.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExternalLinks : MonoBehaviour
+{
+    public void VisitFlowPrivacyPolicy() {
+        Application.OpenURL("https://www.freeprivacypolicy.com/privacy/view/a74170b2b38de6a4e0993da2e2022d2c");
+    }
+
+    public void VisitUnityPrivacyPolicy() {
+        Application.OpenURL("https://unity3d.com/legal/privacy-policy");
+    }
+}
